c9df17c63be99641219ca1b8e7f58f14dd1ea1bb
luis hernandez <lhern026@gmail.com>
2022-12-20 15:48:11 -0800

    fixed logout functionality

--- config/database.js
+++ config/database.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
+
+  useUnifiedTopology: true,
 });
 
 //  shortcut to mongoose.connection object
 const db = mongoose.connection;
 
-db.on('connected', function () {
+db.on("connected", function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
-
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.get('/auth/google', passport.authenticate(
-  'google',
-  { scope: ['profile', 'email'] }
-));
-
-router.get('/oauth2callback', passport.authenticate(
-  'google',
-  {
-    successRedirect: '/',
-    failureRedirect: '/'
-  }
-));
+router.get(
+  "/oauth2callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
 
 // OAuth logout route
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
